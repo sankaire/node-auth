cacd795218a226407170e71e59addd8428139f3f
sankaire <kerenketepela@gmail.com>
2021-11-15 10:44:43 +0300

    create user schema

---
+++ model/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+//schema
+
+const schema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+})
+const User = mongoose.model('User', schema)
+
+module.exports = User
